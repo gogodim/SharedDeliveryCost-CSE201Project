61bd424f0e5af7105a091a67f53531935b1fec5f
maikaedberg <69947279+maikaedberg@users.noreply.github.com>
2020-11-11 15:49:17 +0100

    Update classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -30,6 +30,34 @@ bool check_valid_address(std::string address){
     return true;
 }
 
+Order::Order(User user,
+             Company company,
+             double value,
+             double delivery_cost,
+             double distance){
+        this->user = user;
+        this->company = company;
+        this->value = value;
+        this->delivery_cost = delivery_cost;
+        this->distance = distance;
+}
+
+    User Order::get_user(){
+        return user;
+    }
+    Company Order::get_company(){
+        return company;
+    };
+    double Order::get_value(){
+        return value;
+    };
+    double Order::get_delivery_cost(){
+        return delivery_cost;
+    };
+    double Order::get_distance(){
+        return distance;
+    };
+
 Coordinate convert_to_coordinates(std::string address){
 
     return Coordinate(0,0);
