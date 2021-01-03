e90acbf1264e94e10c60ec39034ad1fe346f6b47
maikaedberg <69947279+maikaedberg@users.noreply.github.com>
2021-01-03 15:36:13 +0900

    Create location.h

---
+++ location.h
@@ -0,0 +1,9 @@
+#ifndef NONWTFUNCTION_H
+#define NONWTFUNCTION_H
+
+#include <classes.h>
+
+#endif // NONWTFUNCTION_H
+
+Coordinate AddressToCoordinate(int argc, char **argv, char* PostalCode, char* City, char* StreetNo);
+string CoordinateToAddress(int argc, char **argv, Coordinate coord);
