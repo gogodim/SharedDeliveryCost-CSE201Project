03f797824b18a4c044e0230ee10beb1a94f882dd
gogodim <56736939+gogodim@users.noreply.github.com>
2021-01-16 13:48:07 +0100

    helping functions comments

--- SDC/SDC/helpingFunctions.h
+++ SDC/SDC/helpingFunctions.h
@@ -12,10 +12,10 @@ public:
     helpingFunctions();
 };
 
-std::string compressComOptions(std::vector< std::vector<double> > compOptions);
-std::vector< std::vector<double> > splitCompOprions(std::string compOptions);
-std::vector<int> splitOrderIDs(std::string OrderIDs);
-std::string compressOrderIDs(std::vector< int > OrderIDs);
-std::string precision_2(float number);
-std::string precision_2(std::string number);
+std::string compressComOptions(std::vector< std::vector<double> > compOptions); //creates a string to be saved in the DB from a 2D vector of orpitons
+std::vector< std::vector<double> > splitCompOprions(std::string compOptions); //decompress the string given by the above function back to a 2D vecor of options
+std::string compressOrderIDs(std::vector< int > OrderIDs); //creates a string to be saved in the DB from a vector of ints
+std::vector<int> splitOrderIDs(std::string OrderIDs); //decompress the string given by the above function to a vector of ints
+std::string precision_2(float number);//creates a string representing a floating point integer with only 2 decimal points
+std::string precision_2(std::string number); //same as above but takes a string instead of a floating point int
 #endif // HELPINGFUNCTIONS_H
